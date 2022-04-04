418f1d716da431ab5894b0570807efeb80a7dd1f
unadulterateddesign <marcus@elevatesoftware.io>
2022-04-03 23:58:33 -0700

    Initial Commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,71 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unadulterated Designs Provides,",
+  headline: "Out Of This World Web Development",
+  description:
+    "Bring your business to the next level with UI/UX designs that offers 100% authenticity and premium customer experience.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../images/svg-1.png"),
+  alt: "car",
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Smooth. Slick. Gorgeous.",
+  headline: "Integrated Perfectly For Your Business ",
+  description:
+    "No matter the platform, we bring the most advanced optimization in the game. Satisfying your customers throughout the entire purchasing process.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../images/svg-5.png"),
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjThree = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Service",
+  headline: "Tier 1 Customer Service & Satisfaction",
+  description:
+    "We work with you throughout the entire build ensuring that we provide the website you desire. It's your world, we are just building it. Click below and contact a representative today!",
+  buttonLabel: "Inquires",
+  imgStart: false,
+  img: require("../images/svg-3.png"),
+  alt: "car",
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+export const homeObjFour = {
+  id: "projects",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Bitcoin. Ethereum. USDC.",
+  headline: "Web 3.0 Integration ",
+  description:
+    "We accept all forms of payments and currently provide Web 3.0 integration to all websites.",
+  buttonLabel: "What is Web 3.0?",
+  imgStart: true,
+  img: require("../images/svg-6.png"),
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import HeroSection from "../components/HeroSection";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import InfoSection from "../components/InfoSection";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+  homeObjFour,
+} from "../components/InfoSection/Data";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjFour} />
+    </>
+  );
+};
+
+export default Home;
